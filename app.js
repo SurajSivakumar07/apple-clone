e22411f9f6e3775fb8c8dfac138357feea33a8f1
Suraj.S <96371899+Surajsuri07@users.noreply.github.com>
2022-11-02 23:54:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+const images=["./images/show1.jpg","./images/show2.jpg","./images/show3.jpg"]
+
+const sliders=document.getElementById("slider")
+
+const clicked0=()=>{
+    sliders.src=images[0]
+}
+const clicked1=()=>{
+    sliders.src=images[1]
+}
+const clicked2=()=>{
+    sliders.src=images[2]
+}
+const clicked3=()=>{
+    sliders.src=images[3]
+}
+
+const clicked4=()=>{
+    sliders.src=images[4]
+}
